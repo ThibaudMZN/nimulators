5c8a7c2a16f7f1fa5a1c5dc276fd2a77e37b8a79
ThibaudMZN <thibaud.marazano@gmail.com>
2024-12-19 19:40:48 -0500

    Adding more CPU instructions

--- chip8/src/chip8.nim
+++ chip8/src/chip8.nim
@@ -1,4 +1,4 @@
-import system
+import std/random
 
 const
   MEMORY_SIZE = 4096
@@ -28,15 +28,16 @@ const
 type
   Chip8* = object
     memory*: array[MEMORY_SIZE, uint8]
-    registers: array[16, uint8]
-    I: uint16
+    registers*: array[16, uint8]
+    I*: uint16
     pc*: uint16
     display*: array[DISPLAY_WIDTH * DISPLAY_HEIGHT, bool]
     delayTimer: uint8
     soundTimer: uint8
-    stack: array[16, uint16]
-    stackPointer: uint16
+    stack*: array[16, uint16]
+    stackPointer*: uint16
     keypad: array[16, bool]
+    rng*: Rand
 
 proc initChip8*(): Chip8 =
   result = Chip8(
@@ -49,12 +50,19 @@ proc initChip8*(): Chip8 =
     soundTimer: 0,
     stack: default(array[16, uint16]),
     stackPointer: 0,
-    keypad: default(array[16, bool])
+    keypad: default(array[16, bool]),
+    rng: initRand()
   )
   result.memory[0 ..< FONTSET.len] = FONTSET
 
 proc incrementPc*(chip8: var Chip8) = 
   chip8.pc += 2
+
+proc currentStack(chip8: var Chip8): uint16 =
+  chip8.stack[chip8.stackPointer]
+
+proc `currentStack=`(chip8: var Chip8, stack: uint16) =
+  chip8.stack[chip8.stackPointer] = stack
 
 proc executeOp*(chip8: var Chip8, opcode: uint16) =
   case opcode shr 12:
@@ -62,8 +70,87 @@ proc executeOp*(chip8: var Chip8, opcode: uint16) =
       if opcode == 0x00E0:
         chip8.display = default(array[DISPLAY_WIDTH * DISPLAY_HEIGHT, bool])
         chip8.incrementPc()
+      elif opcode == 0x00EE:
+        chip8.stackPointer -= 1
+        chip8.pc = chip8.currentStack
     of 0x1:
       chip8.pc = opcode and 0x0FFF
+    of 0x2:
+      chip8.currentStack = chip8.pc
+      chip8.stackPointer += 1
+      chip8.pc = opcode and 0x0FFF
+    of 0x3:
+      var x = (opcode and 0x0F00) shr 8
+      if chip8.registers[x] == (opcode and 0x00FF):
+        chip8.incrementPc()
+      chip8.incrementPc()
+    of 0x4:
+      var x = (opcode and 0x0F00) shr 8
+      if chip8.registers[x] != (opcode and 0x00FF):
+        chip8.incrementPc()
+      chip8.incrementPc()
+    of 0x5:
+      var x = (opcode and 0x0F00) shr 8
+      var y = (opcode and 0x00F0) shr 4
+      if chip8.registers[x] == chip8.registers[y]:
+        chip8.incrementPc()
+      chip8.incrementPc()
+    of 0x6:
+      var x = (opcode and 0x0F00) shr 8
+      chip8.registers[x] = uint8(opcode and 0x00FF)
+      chip8.incrementPc()
+    of 0x7:
+      var x = (opcode and 0x0F00) shr 8
+      chip8.registers[x] += uint8(opcode and 0x00FF)
+      chip8.incrementPc()
+    of 0x8:
+      var x = (opcode and 0x0F00) shr 8
+      var y = (opcode and 0x00F0) shr 4
+      var m = (opcode and 0x000F)
+      case m:
+        of 0:
+          chip8.registers[x] = chip8.registers[y]
+        of 1:
+          chip8.registers[x] = chip8.registers[x] or chip8.registers[y]
+        of 2:
+          chip8.registers[x] = chip8.registers[x] and chip8.registers[y]
+        of 3:
+          chip8.registers[x] = chip8.registers[x] xor chip8.registers[y]
+        of 4:
+          chip8.registers[0xF] = uint8(uint16(chip8.registers[x]) + uint16(chip8.registers[y]) > 0xFF)
+          chip8.registers[x] = chip8.registers[x] + chip8.registers[y]
+        of 5:
+          chip8.registers[0xF] = uint8(chip8.registers[x] > chip8.registers[y])
+          chip8.registers[x] = chip8.registers[x] - chip8.registers[y]
+        of 6:
+          chip8.registers[0xF] = chip8.registers[x] and 0b00000001
+          chip8.registers[x] = chip8.registers[x] shr 1
+        of 7:
+          chip8.registers[0xF] = uint8(chip8.registers[y] > chip8.registers[x])
+          chip8.registers[x] = chip8.registers[y] - chip8.registers[x]
+        of 0xE:
+          chip8.registers[0xF] = (chip8.registers[x] and 0b10000000) shr 7
+          chip8.registers[x] = chip8.registers[x] shl 1
+        else:
+          echo "Unknown mode for ALU 0x8, mode: ", $m
+      chip8.incrementPc()
+    of 0x9:
+      var x = (opcode and 0x0F00) shr 8
+      var y = (opcode and 0x00F0) shr 4
+      if chip8.registers[x] != chip8.registers[y]:
+        chip8.incrementPc()
+      chip8.incrementPc()
+    of 0xA:
+      chip8.I = (opcode and 0x0FFF)
+      chip8.incrementPc()
+    of 0xB:
+      chip8.pc = (opcode and 0x0FFF) + chip8.registers[0]
+    of 0xC:
+      var x = (opcode and 0x0F00) shr 8
+      var kk = uint8(opcode and 0x00FF)
+      var r = uint8(chip8.rng.rand(255))
+      chip8.registers[x] = r and kk
+      chip8.incrementPc()
     else:
       echo "Unknown opcode: ", $opcode
 
--- chip8/tests/t_chip8.nim
+++ chip8/tests/t_chip8.nim
@@ -1,4 +1,5 @@
 import unittest
+import std/random
 import ../src/chip8
 
 suite "The Chip8 emulator":
@@ -13,13 +14,196 @@ suite "The Chip8 emulator":
     chip8.incrementPc()
     check(chip8.pc == 0x202) # Since we start at 0x200
 
-  suite "when executing instructions":
-      test "can handle 00E0 (Clear display)":
+  suite "when executing instructions, can handle":
+      test "00E0 (Clear display)":
         chip8.display[0] = true
         chip8.executeOp(0x00E0)
         check(chip8.display[0] == false)
         check(chip8.pc == 0x202)
 
-      test "can handle 1nnn (Jump to location)":
+      test "00EE (Return from subroutine)":
+        chip8.stack[0] = 0x242
+        chip8.stackPointer = 1
+        chip8.executeOp(0x00EE)
+        check(chip8.pc == 0x242)
+        check(chip8.stackPointer == 0)
+
+      test "1nnn (Jump to location)":
         chip8.executeOp(0x1042)
         check(chip8.pc == 0x042)
+
+      test "2nnn (Call subroutine)":
+        chip8.executeOp(0x2042)
+        check(chip8.stackPointer == 1)
+        check(chip8.stack[0] == 0x200)
+        check(chip8.pc == 0x042)
+
+      test "3xkk (Skip next instruction - equal)":
+        chip8.registers[1] = 0x42
+        chip8.executeOp(0x3142)
+        check(chip8.pc == 0x204) # Has incremented PC twice
+
+        chip8.registers[1] = 0x64
+        chip8.executeOp(0x3142)
+        check(chip8.pc == 0x206) # Has incremented PC only once since 64 != 42
+
+      test "4xkk (Skip next instruction - not equal)":
+        chip8.registers[1] = 0x42
+        chip8.executeOp(0x4142)
+        check(chip8.pc == 0x202) # Has incremented PC only once since 42 == 42
+
+        chip8.registers[1] = 0x64
+        chip8.executeOp(0x4142)
+        check(chip8.pc == 0x206) # Has incremented PC twice since 64 != 42
+
+      test "5xy0 (Skip next instruction - registers equal)":
+        chip8.registers[1] = 0x42
+        chip8.registers[2] = 0x42
+        chip8.executeOp(0x5120)
+        check(chip8.pc == 0x204) # Has incremented PC twice
+
+        chip8.registers[1] = 0x64
+        chip8.executeOp(0x5120)
+        check(chip8.pc == 0x206) # Has incremented PC only once since 64 != 42
+
+      test "6xkk (Load)":
+        chip8.executeOp(0x6042)
+        check(chip8.registers[0] == 0x42)
+        check(chip8.pc == 0x202)
+
+      test "7xkk (Add)":
+        chip8.registers[0] = 0x42
+        chip8.executeOp(0x7042)
+        check(chip8.registers[0] == 0x84)
+        check(chip8.pc == 0x202)
+
+        # Make sure that overflow is not a problem (expected behavior)
+        chip8.registers[0] = 0xFF
+        chip8.executeOp(0x7002)
+        check(chip8.registers[0] == 0x01)
+        check(chip8.pc == 0x204)
+
+      test "8xy0 (Load from register)":
+        chip8.registers[1] = 0x42
+        chip8.executeOp(0x8010)
+        check(chip8.registers[0] == 0x42)
+        check(chip8.pc == 0x202)
+
+      test "8xy1 (OR from register)":
+        chip8.registers[0] = 0x01
+        chip8.registers[1] = 0x10
+        chip8.executeOp(0x8011)
+        check(chip8.registers[0] == 0x11)
+        check(chip8.pc == 0x202)
+
+      test "8xy2 (AND from register)":
+        chip8.registers[0] = 0x11
+        chip8.registers[1] = 0x10
+        chip8.executeOp(0x8012)
+        check(chip8.registers[0] == 0x10)
+        check(chip8.pc == 0x202)
+
+      test "8xy3 (XOR from register)":
+        chip8.registers[0] = 0x11
+        chip8.registers[1] = 0x10
+        chip8.executeOp(0x8013)
+        check(chip8.registers[0] == 0x01)
+        check(chip8.pc == 0x202)
+
+      test "8xy4 (ADD from register with carry flag)":
+        chip8.registers[0] = 0xFF
+        chip8.registers[1] = 0x02
+        chip8.executeOp(0x8014)
+        check(chip8.registers[0] == 0x01)
+        check(chip8.registers[0xF] == 0x01)
+        check(chip8.pc == 0x202)
+
+        chip8.registers[0] = 0x42
+        chip8.registers[1] = 0x42
+        chip8.executeOp(0x8014)
+        check(chip8.registers[0] == 0x84)
+        check(chip8.registers[0xF] == 0)
+        check(chip8.pc == 0x204)
+
+      test "8xy5 (SUB from register with NOT borrow flag)":
+        chip8.registers[0] = 0xFF
+        chip8.registers[1] = 0x01
+        chip8.executeOp(0x8015)
+        check(chip8.registers[0] == 0xFE)
+        check(chip8.registers[0xF] == 0x01)
+        check(chip8.pc == 0x202)
+
+        chip8.registers[0] = 0x01
+        chip8.registers[1] = 0x02
+        chip8.executeOp(0x8015)
+        check(chip8.registers[0] == 0xFF)
+        check(chip8.registers[0xF] == 0)
+        check(chip8.pc == 0x204)
+
+      test "8xy6 (Least significant bit)":
+        chip8.registers[0] = 0x03
+        chip8.executeOp(0x8016)
+        check(chip8.registers[0] == 0x01)
+        check(chip8.registers[0xF] == 0x01)
+        check(chip8.pc == 0x202)
+
+        chip8.registers[0] = 0x02
+        chip8.executeOp(0x8016)
+        check(chip8.registers[0] == 0x01)
+        check(chip8.registers[0xF] == 0)
+        check(chip8.pc == 0x204)
+
+      test "8xy7 (SUB from register with NOT borrow flag Vy - Vx)":
+        chip8.registers[0] = 0x01
+        chip8.registers[1] = 0xFF
+        chip8.executeOp(0x8017)
+        check(chip8.registers[0] == 0xFE)
+        check(chip8.registers[0xF] == 0x01)
+        check(chip8.pc == 0x202)
+
+        chip8.registers[0] = 0xFF
+        chip8.registers[1] = 0x01
+        chip8.executeOp(0x8017)
+        check(chip8.registers[0] == 0x02)
+        check(chip8.registers[0xF] == 0)
+        check(chip8.pc == 0x204)
+
+      test "8xyE (Most significant bit)":
+        chip8.registers[0] = 0xF0
+        chip8.executeOp(0x801E)
+        check(chip8.registers[0] == 0xE0)
+        check(chip8.registers[0xF] == 0x01)
+        check(chip8.pc == 0x202)
+
+        chip8.registers[0] = 0x01
+        chip8.executeOp(0x801E)
+        check(chip8.registers[0] == 0x02)
+        check(chip8.registers[0xF] == 0)
+        check(chip8.pc == 0x204)
+
+      test "9xy0 (Skip next instruction - registers not equal)":
+        chip8.registers[1] = 0x42
+        chip8.registers[2] = 0x42
+        chip8.executeOp(0x9120)
+        check(chip8.pc == 0x202) # Has incremented PC only once since 42 != 42
+
+        chip8.registers[1] = 0x64
+        chip8.executeOp(0x9120)
+        check(chip8.pc == 0x206) # Has incremented PC twice
+
+      test "Annn (Set index)":
+        chip8.executeOp(0xA042)
+        check(chip8.I == 0x42)
+        check(chip8.pc == 0x202)
+
+      test "Bnnn (Jump to V0 + nnn)":
+        chip8.registers[0] = 0x42
+        chip8.executeOp(0xB042)
+        check(chip8.pc == 0x84)
+
+      test "Cxkk (Random byte AND kk)":
+        var rng = initRand(1) # Will always give 251 as first output
+        chip8.rng = rng
+        chip8.executeOp(0xC010)
+        check(chip8.registers[0] == 0x10)
+        check(chip8.pc == 0x202)
