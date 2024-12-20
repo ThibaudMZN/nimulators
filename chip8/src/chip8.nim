6c11221217c571d6b305b4c706af4f5c568e6577
ThibaudMZN <thibaud.marazano@gmail.com>
2024-12-19 23:53:19 -0500

    Finishing CPU instructions

--- chip8/src/chip8.nim
+++ chip8/src/chip8.nim
@@ -1,4 +1,6 @@
 import std/random
+import os
+import std/strutils
 
 const
   MEMORY_SIZE = 4096
@@ -32,12 +34,13 @@ type
     I*: uint16
     pc*: uint16
     display*: array[DISPLAY_WIDTH * DISPLAY_HEIGHT, bool]
-    delayTimer: uint8
-    soundTimer: uint8
+    delayTimer*: uint8
+    soundTimer*: uint8
     stack*: array[16, uint16]
     stackPointer*: uint16
-    keypad: array[16, bool]
+    keypad*: array[16, bool]
     rng*: Rand
+    drawFlag*: bool
 
 proc initChip8*(): Chip8 =
   result = Chip8(
@@ -51,7 +54,8 @@ proc initChip8*(): Chip8 =
     stack: default(array[16, uint16]),
     stackPointer: 0,
     keypad: default(array[16, bool]),
-    rng: initRand()
+    rng: initRand(),
+    drawFlag: false
   )
   result.memory[0 ..< FONTSET.len] = FONTSET
 
@@ -65,19 +69,24 @@ proc `currentStack=`(chip8: var Chip8, stack: uint16) =
   chip8.stack[chip8.stackPointer] = stack
 
 proc executeOp*(chip8: var Chip8, opcode: uint16) =
+  chip8.drawFlag = false
   case opcode shr 12:
     of 0x0:
       if opcode == 0x00E0:
         chip8.display = default(array[DISPLAY_WIDTH * DISPLAY_HEIGHT, bool])
         chip8.incrementPc()
+        chip8.drawFlag = true
       elif opcode == 0x00EE:
-        chip8.stackPointer -= 1
+        dec(chip8.stackPointer)
         chip8.pc = chip8.currentStack
+        chip8.incrementPc()
+      else:
+        echo "Unknown opcode 0x0", $opcode
     of 0x1:
       chip8.pc = opcode and 0x0FFF
     of 0x2:
       chip8.currentStack = chip8.pc
-      chip8.stackPointer += 1
+      inc(chip8.stackPointer)
       chip8.pc = opcode and 0x0FFF
     of 0x3:
       var x = (opcode and 0x0F00) shr 8
@@ -97,7 +106,8 @@ proc executeOp*(chip8: var Chip8, opcode: uint16) =
       chip8.incrementPc()
     of 0x6:
       var x = (opcode and 0x0F00) shr 8
-      chip8.registers[x] = uint8(opcode and 0x00FF)
+      var kk = uint8(opcode and 0x00FF)
+      chip8.registers[x] = kk
       chip8.incrementPc()
     of 0x7:
       var x = (opcode and 0x0F00) shr 8
@@ -151,9 +161,100 @@ proc executeOp*(chip8: var Chip8, opcode: uint16) =
       var r = uint8(chip8.rng.rand(255))
       chip8.registers[x] = r and kk
       chip8.incrementPc()
+    of 0xD:
+      var n = opcode and 0x000F
+      chip8.registers[0xF] = 0
+      var regX = chip8.registers[(opcode and 0x0F00) shr 8]
+      var regY = chip8.registers[(opcode and 0x00F0) shr 4]
+      var y = uint16(0)
+      while y < n:
+        var spr = chip8.memory[chip8.I + y];
+        var x = uint16(0)
+        while x < 8:
+          const msb = uint8(0x80)
+          if (spr and (msb shr x)) != 0:
+              var tX = (regX + x) mod 64
+              var tY = (regY + y) mod 32
+              var idx = tX + tY * 64
+              chip8.display[idx] = chip8.display[idx] xor true;
+              if chip8.display[idx] == false:
+                chip8.registers[0x0F] = 1
+          x += 1
+        y += 1
+      chip8.drawFlag = true
+      chip8.incrementPc()
+    of 0xE:
+      var x = (opcode and 0x0F00) shr 8
+      var m = (opcode and 0x00FF)
+      case m:
+        of 0x9e:
+          if chip8.keypad[chip8.registers[x]]:
+            chip8.incrementPc()
+        of 0xA1:
+          if not chip8.keypad[chip8.registers[x]]:
+            chip8.incrementPc()
+        else:
+          echo "Unknown mode for key handling 0xE, mode: ", $m
+      chip8.incrementPc()
+    of 0xF:
+      var x = (opcode and 0x0F00) shr 8
+      var m = (opcode and 0x00FF)
+      case m:
+        of 0x07:
+          chip8.registers[x] = chip8.delayTimer
+        of 0x0A:
+          var key_pressed = false;
+          var i = uint8(0)
+          while i < 16:
+            if chip8.keypad[i]:
+              chip8.registers[x] = i
+              key_pressed = true
+              break
+            i += 1
+                    
+          if not key_pressed:
+            return
+        of 0x15:
+          chip8.delayTimer = chip8.registers[x]
+        of 0x18:
+          chip8.soundTimer = chip8.registers[x]
+        of 0x1E:
+          var vx = chip8.registers[x]
+          chip8.registers[0x0f] = if chip8.I + vx > uint16(0xfff): 1 else: 0
+          chip8.I += vx
+        of 0x29:
+          chip8.I = chip8.registers[x] * 0x5
+        of 0x33:
+          chip8.memory[chip8.I] = chip8.registers[x] div 100
+          chip8.memory[chip8.I + 1] = (chip8.registers[x] div 10) mod 10
+          chip8.memory[chip8.I + 2] = chip8.registers[x] mod 10
+        of 0x55:
+          var i = uint8(0)
+          while i <= x:
+            chip8.memory[chip8.I + i] = chip8.registers[i];
+            i += 1
+        of 0x65:
+          var i = uint8(0)
+          while i <= x:
+            chip8.registers[i] = chip8.memory[chip8.I + i]
+            i += 1
+        else:
+          echo "Unknown mode for misc handling 0xF, mode: ", $m
+      chip8.incrementPc()
     else:
       echo "Unknown opcode: ", $opcode
 
 proc cycle*(chip8: var Chip8) =
-  var opcode = uint16(chip8.memory[chip8.pc] shl 8 or chip8.memory[chip8.pc + 1])
+  var opcode = (uint16(chip8.memory[chip8.pc]) shl 8) or chip8.memory[chip8.pc + 1]
   chip8.executeOp(opcode)
+  if chip8.delayTimer > 0:
+    chip8.delayTimer -= 1
+
+  if chip8.soundTimer > 0:
+    chip8.soundTimer -= 1
+
+proc loadROM*(chip8: var Chip8, romPath: string) =
+  if not fileExists(romPath):
+    raise newException(IOError, "ROM file not found!")
+  let romData = readFile(romPath)
+  chip8.memory[0x200 ..< 0x200 + romData.len] = cast[seq[byte]](romData)
--- chip8/tests/t_chip8.nim
+++ chip8/tests/t_chip8.nim
@@ -25,7 +25,7 @@ suite "The Chip8 emulator":
         chip8.stack[0] = 0x242
         chip8.stackPointer = 1
         chip8.executeOp(0x00EE)
-        check(chip8.pc == 0x242)
+        check(chip8.pc == 0x244)
         check(chip8.stackPointer == 0)
 
       test "1nnn (Jump to location)":
@@ -207,3 +207,89 @@ suite "The Chip8 emulator":
         chip8.executeOp(0xC010)
         check(chip8.registers[0] == 0x10)
         check(chip8.pc == 0x202)
+
+      test "Dxyn (Display n-byte sprite)":
+        # I is 0, so we should draw what is at memory[0], which is the first byte of the "0" from the fontset
+        chip8.executeOp(0xD001)
+        check(chip8.display[0] == true)
+        check(chip8.registers[0x0F] == 0)
+        check(chip8.drawFlag == true)
+        check(chip8.pc == 0x202)
+
+        # Now if we execute the same command, we should "erase" the 0
+        chip8.executeOp(0xD001)
+        check(chip8.display[0] == false)
+        check(chip8.registers[0x0F] == 1)
+        check(chip8.pc == 0x204)
+
+      test "Ex9E (Skip if key pressed)":
+        chip8.registers[0] = 0x01
+        chip8.keypad[0x01] = true
+        chip8.executeOp(0xE09E)
+        check(chip8.pc == 0x204) # Skipped because key is pressed
+
+        chip8.keypad[0x01] = false
+        chip8.executeOp(0xE09E)
+        check(chip8.pc == 0x206) # Not skipped because key is not pressed
+
+      test "ExA1 (Skip if key not pressed)":
+        chip8.registers[0] = 0x01
+        chip8.keypad[0x01] = false
+        chip8.executeOp(0xE0A1)
+        check(chip8.pc == 0x204) # Skipped because key is not pressed
+
+        chip8.keypad[0x01] = true
+        chip8.executeOp(0xE0A1)
+        check(chip8.pc == 0x206) # Not skipped because key is pressed
+
+      test "Fx07 (Load Delay timer)":
+        chip8.delayTimer = 0x42
+        chip8.executeOp(0xF007)
+        check(chip8.registers[0] == 0x42)
+
+      test "Fx0A (Wait for key press)":
+        chip8.keypad[0x02] = true
+        chip8.executeOp(0xF00A)
+        check(chip8.registers[0] == 0x02)
+
+      test "Fx15 (Set Delay timer)":
+        chip8.registers[0] = 0x42
+        chip8.executeOp(0xF015)
+        check(chip8.delayTimer == 0x42)
+
+      test "Fx18 (Set Delay timer)":
+        chip8.registers[0] = 0x42
+        chip8.executeOp(0xF018)
+        check(chip8.soundTimer == 0x42)
+
+      test "Fx1E (Add index)":
+        chip8.I = 0x01
+        chip8.registers[0] = 0x42
+        chip8.executeOp(0xF01E)
+        check(chip8.I == 0x43)
+
+      test "Fx29 (Set index to sprite location)":
+        chip8.registers[0] = 0x03
+        chip8.executeOp(0xF029)
+        check(chip8.I == 15) # Since each font char is 5 byte
+
+      test "Fx33 (Store BCD representation)":
+        chip8.registers[0] = 123
+        chip8.executeOp(0xF033)
+        check(chip8.memory[0] == 1)
+        check(chip8.memory[1] == 2)
+        check(chip8.memory[2] == 3)
+
+      test "Fx55 (Store registers)":
+        chip8.registers[0] = 0x42
+        chip8.registers[1] = 0x84
+        chip8.executeOp(0xF155)
+        check(chip8.memory[0] == 0x42)
+        check(chip8.memory[1] == 0x84)
+
+      test "Fx65 (Store registers)":
+        chip8.memory[0] = 0x42
+        chip8.memory[1] = 0x84
+        chip8.executeOp(0xF165)
+        check(chip8.registers[0] == 0x42)
+        check(chip8.registers[1] == 0x84)
